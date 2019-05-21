0083653eec0a0383468999374717abe26ff3d847
Kaisyuan <2633147589@qq.com>
2019-05-21 18:01:33 +0800

    修改架构

--- Admin/src/components/HelloWorld.vue
+++ Admin/src/components/HelloWorld.vue
@@ -1,60 +1,44 @@
-<template>
+<template >
   <div class="layout">
     <Layout>
-      <Header>
-        <Menu mode="horizontal" theme="dark" active-name="1">
-          <div class="layout-logo"></div>
+        <Menu mode="horizontal" class="menu-color" active-name="1">
+          <div class="layout-logo">kaisyuan</div>
           <div class="layout-nav">
-            <MenuItem name="1">
-              <Icon type="ios-navigate"></Icon>Item 1
-            </MenuItem>
-            <MenuItem name="2">
-              <Icon type="ios-keypad"></Icon>Item 2
-            </MenuItem>
-            <MenuItem name="3">
-              <Icon type="ios-analytics"></Icon>Item 3
-            </MenuItem>
-            <MenuItem name="4">
-              <Icon type="ios-paper"></Icon>Item 4
-            </MenuItem>
+            <ul class="menu-ul">
+              <li><Icon type="logo-github" /></li>
+              <li><Avatar src="https://i.loli.net/2017/08/21/599a521472424.jpg" /></li>
+            </ul>
           </div>
         </Menu>
-      </Header>
       <Layout>
         <Sider hide-trigger :style="{background: '#fff'}">
           <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
             <Submenu name="1">
               <template slot="title">
-                <Icon type="ios-navigate"></Icon>Item 1
+                <Icon type="ios-navigate"></Icon>用户管理
               </template>
-              <MenuItem name="1-1">Option 1</MenuItem>
-              <MenuItem name="1-2">Option 2</MenuItem>
-              <MenuItem name="1-3">Option 3</MenuItem>
+              <MenuItem name="1-1">人员列表</MenuItem>
+              <MenuItem name="1-2">其他</MenuItem>
+              <MenuItem name="1-3">其他</MenuItem>
             </Submenu>
             <Submenu name="2">
               <template slot="title">
-                <Icon type="ios-keypad"></Icon>Item 2
+                <Icon type="ios-keypad"></Icon>文章管理
               </template>
-              <MenuItem name="2-1">Option 1</MenuItem>
-              <MenuItem name="2-2">Option 2</MenuItem>
+              <MenuItem name="2-1">文章列表</MenuItem>
+              <MenuItem name="2-2">发布文章</MenuItem>
+              <MenuItem name="2-2">文章草稿</MenuItem>
             </Submenu>
             <Submenu name="3">
               <template slot="title">
-                <Icon type="ios-analytics"></Icon>Item 3
+                <Icon type="ios-analytics"></Icon>其他
               </template>
               <MenuItem name="3-1">Option 1</MenuItem>
               <MenuItem name="3-2">Option 2</MenuItem>
             </Submenu>
           </Menu>
         </Sider>
-        <Layout :style="{padding: '0 24px 24px'}">
-          <Breadcrumb :style="{margin: '24px 0'}">
-            <BreadcrumbItem>Home</BreadcrumbItem>
-            <BreadcrumbItem>Components</BreadcrumbItem>
-            <BreadcrumbItem>Layout</BreadcrumbItem>
-          </Breadcrumb>
-          <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">Content</Content>
-        </Layout>
+
       </Layout>
     </Layout>
   </div>
@@ -64,8 +48,173 @@
 export default {
   name: "HelloWorld",
   data() {
-    return {}
-  }
+    return {
+      search:{
+        name:'',
+        address:'',
+        age:''
+      },
+      columns: [
+        {
+          type: 'selection',
+          width: 60,
+          align: 'center'
+        },
+        {
+          title: 'Number',
+          key: 'number'
+        },
+        {
+          title: 'Name',
+          key: 'name'
+        },
+        {
+          title: 'Age',
+          key: 'age'
+        },
+        {
+          title: 'Address',
+          key: 'address'
+        },
+        {
+          title: 'Date',
+          key: 'date'
+        },
+        {
+          title: 'Action',
+          key: 'action',
+          width: 180,
+          align: 'center',
+          render: (h, params) => {
+            return h('div', [
+              h('Button', {
+                props: {
+                  type: 'info',
+                  size: 'small'
+                },
+                style: {
+                  marginRight: '5px'
+                },
+                on: {
+                  click: () => {
+                    this.show(params.index)
+                  }
+                }
+              }, '查看'),
+              h('Button', {
+                props: {
+                  type: 'primary',
+                  size: 'small'
+                },
+                style: {
+                  marginRight: '5px'
+                },
+                on: {
+                  click: () => {
+                    this.remove(params.index)
+                  }
+                }
+              }, '修改'),
+              h('Button', {
+                props: {
+                  type: 'error',
+                  size: 'small'
+                },
+                on: {
+                  click: () => {
+                    this.remove(params.index)
+                  }
+                }
+              }, '删除')
+            ]);
+          }
+        }
+      ],
+      data1: [
+        {
+          number: 1,
+          name: 'John Brown',
+          age: 18,
+          address: 'New York No. 1 Lake Park',
+          date: '2019-05-21'
+        },
+        {
+          number: 2,
+          name: 'Jim Green',
+          age: 24,
+          address: 'London No. 1 Lake Park',
+          date: '2019-05-21'
+        },
+        {
+          number: 3,
+          name: 'Joe Black',
+          age: 30,
+          address: 'Sydney No. 1 Lake Park',
+          date: '2019-05-21'
+        },
+        {
+          number: 4,
+          name: 'Jon Snow',
+          age: 26,
+          address: 'Ottawa No. 2 Lake Park',
+          date: '2019-05-21'
+        },
+        {
+          number: 5,
+          name: 'Jon Snow',
+          age: 26,
+          address: 'Ottawa No. 2 Lake Park',
+          date: '2019-05-21'
+        },
+        {
+          number: 6,
+          name: 'Jon Snow',
+          age: 26,
+          address: 'Ottawa No. 2 Lake Park',
+          date: '2019-05-21'
+        },
+        {
+          number: 7,
+          name: 'Jon Snow',
+          age: 26,
+          address: 'Ottawa No. 2 Lake Park',
+          date: '2019-05-21'
+        },
+        {
+          number: 8,
+          name: 'Jon Snow',
+          age: 26,
+          address: 'Ottawa No. 2 Lake Park',
+          date: '2019-05-21'
+        },
+        {
+          number: 9,
+          name: 'Jon Snow',
+          age: 26,
+          address: 'Ottawa No. 2 Lake Park',
+          date: '2019-05-21'
+        },
+        {
+          number: 10,
+          name: 'Jon Snow',
+          age: 26,
+          address: 'Ottawa No. 2 Lake Park',
+          date: '2019-05-21'
+        }
+      ]
+    }
+  },
+  methods: {
+    handleSelectAll (status) {
+      this.$refs.selection.selectAll(status);
+    },
+    reset (){
+      this.search.name = '';
+      this.search.address ='';
+      this.search.age ='';
+
+    }
+}
 }
 </script>
 
@@ -81,16 +230,27 @@ export default {
 .layout-logo {
   width: 100px;
   height: 30px;
-  background: #5b6270;
+  background: #ff9900ff;
   border-radius: 3px;
   float: left;
   position: relative;
   top: 15px;
   left: 20px;
 }
-.layout-nav {
-  width: 420px;
-  margin: 0 auto;
+.menu-ul{
+  list-style: none;
+  float: right;
   margin-right: 20px;
+  text-align: center;
+}
+.menu-ul li{
+  float: left;
+  padding-left: 20px;
+}
+.menu-color{
+  background-color: white;
+}
+.ivu-layout{
+  height: 100%;
 }
 </style>
