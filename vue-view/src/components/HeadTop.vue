d18765c0d1a06893244b997597775518916618ba
kaisyuan <2633147589@qq.com>
2021-06-11 11:17:49 +0800

    更新

---
+++ vue-view/src/components/HeadTop.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="head">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo hidden-xs-only"
+      mode="horizontal"
+      @select="handleSelect"
+    >
+      <el-menu-item index="1">主页</el-menu-item>
+      <el-menu-item index="2">文章</el-menu-item>
+      <el-menu-item index="3">照片</el-menu-item>
+      <el-submenu index="4">
+        <template slot="title">福利其他</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="5" disabled>日常</el-menu-item>
+      <el-menu-item index="6">
+        <a target="_blank">关于我</a>
+      </el-menu-item>
+      <!--<div class="scan">-->
+        <el-input placeholder="请输入内容" v-model="input3" class="input-with-select" @blur="blur">
+          <el-button slot="append" icon="el-icon-search" class="search"></el-button>
+        </el-input>
+      <!--</div>-->
+    </el-menu>
+    <el-menu
+      default-active="1-4-1"
+      class="el-menu-vertical-demo hidden-sm-only hidden-md-only hidden-lg-only hidden-xl-only"
+      @open="handleOpen"
+      @close="handleClose"
+      :collapse="isCollapse"
+    >
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-s-unfold"></i>
+        </template>
+        <el-menu-item index="1-1">主页</el-menu-item>
+        <el-menu-item index="1-2">文章</el-menu-item>
+        <el-menu-item index="1-3">照片</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">福利其他</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="1-4">日常</el-menu-item>
+        <el-menu-item index="1-5">关于我</el-menu-item>
+      </el-submenu>
+    </el-menu>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'headtop',
+  data () {
+    return {
+      isCollapse: true
+    }
+  },
+  methods: {
+    blur: function (event) {
+      console.log(event)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+
+.el-menu-demo {
+  width: 100%;
+  padding-left: 20%;
+  position: fixed;
+  z-index: 99;
+}
+.el-menu-vertical-demo {
+  position: fixed;
+  z-index: 999;
+}
+.input-with-select{
+  margin-top: 10px;
+  width: 20%;
+  height: 10px;
+}
+.el-input__inner{
+  width: 30%;
+  height: 32px;
+}
+</style>
---
+++ vue-view/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/display.css'
+
+Vue.use(iView)
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+let vue = new Vue({
+  el: '#app',
+  components: { App },
+  render: h => h(App),
+  template: '<App/>'
+})
+Vue.use({
+  vue
+})
